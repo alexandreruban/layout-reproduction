f97f6a8472b352649550b7f187e61c9e26ce1815
Alexandre Ruban <alexandre@hey.com>
2022-07-04 20:49:12 +0200

    No layout for Turbo Frame requests that are not Turbo Stream requests
    In this commit, we achieve the best of both worlds.
    When clicking on the "New article" link, we don't have a layout anymore
    which is what we want:
    
    ```html
    <h1>New article</h1>
    
    <a href="/articles">Back to articles</a>
    
    <turbo-frame id="new_article">
      <form action="/articles" accept-charset="UTF-8" method="post">
        # ...
      </form>
    </turbo-frame>
    ```
    
    We still have the layout when we click on the "Create Article" button:
    
    ```html
    <turbo-stream action="update" target="flash">
      <template>
        <div>
          Article was successfully created.
        </div>
      </template>
    </turbo-stream>
    
    <turbo-stream action="update" target="new_article"><template></template></turbo-stream>
    
    <turbo-stream action="append" target="articles">
      <template>
        <div>
          Article #10: Whooh
        </div>
      </template>
    </turbo-stream>
    ```

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  layout "application"
+  layout -> { (turbo_frame_request? && !turbo_stream_request?) ? false : "application" }
+
+  private
+
+  def turbo_stream_request?
+    request.accept.starts_with?(Mime[:turbo_stream])
+  end
 end
